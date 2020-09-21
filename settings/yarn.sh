35d94431f3a94477e4c5e2a1447e51748f9945b5
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-09-21 14:01:07 +0700

    Update yarn.sh

--- settings/yarn.sh
+++ settings/yarn.sh
@@ -2,9 +2,6 @@
 
 _myzs:internal:module:initial "$0"
 
-export NODE_LTS_PATH="/usr/local/opt/node@12/bin"
-_myzs:internal:path-append "$NODE_LTS_PATH"
-
 if _myzs:internal:checker:command-exist "yarn"; then
   export CYARN_BIN="$(yarn global bin)"
   _myzs:internal:path-append "$CYARN_BIN"
